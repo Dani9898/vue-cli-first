5a130049f54134c538c77f7876b371e227bb7cd6
Dani9898 <danieleoliva98@gmail.com>
2021-11-23 15:57:01 +0100

    aggiunto banner

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-
+    <Banner/>
     <Title msg="La mia prima app con Vue CLI!" />
   </div>
 </template>
 
 <script>
-import Title from './components/Titolo.vue'
+import Title from './components/first-cli.vue'
+import Banner from './components/hero.vue'
+
 
 export default {
   name: 'App',
   components: {
-    Title
+    Title,
+    Banner
   }
 }
 </script>
---
+++ vue-cli-first/src/components/hero.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="jumbo">
+    <img src="https://giffiles.alphacoders.com/208/208553.gif" alt="">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.jumbo {
+    width: 70%;
+    margin: auto;
+}
+img {
+    width: 100%;
+
+}
+</style>
